0e0b50edb410c1b550b69557ff4543134a80717d
perryharlock <perry.harlock@tiscali.co.uk>
2013-08-07 16:37:24 +0100

    Refactor....all the things

--- js/script.js
+++ js/script.js
@@ -3,37 +3,61 @@ $(document).ready(function(){
 	$("body.no-javascript").removeClass("no-javascript");
 
   // Elements
-  var row_template = '[data-role=row-template]';
-  var add_button = "[data-role='add-button']";
-  var delete_button = "[data-role='delete-button']";
-  var edit_button = "[data-role='edit-button']";
-  var to_do_list = "[data-role='to-do-list']";
-  var entry_count = 0;
+  var addButton = "[data-role='add-button']";
+  var deleteButton = "[data-role='delete-button']";
+  var editButton = "[data-role='edit-button']";
+  var saveButton = "[data-role='save-button']";
+  var cancelButton = "[data-role='cancel-button']";
+  var buttons = ".button";
+  var toDoList = "[data-role='to-do-list']";
+  var entryCount = 0;
+  var $inputDescription = "input.description";
+  var $spanDescription = "span.description";
+  var $inputDeadline = "input.deadline";
+  var $spanDeadline = "span.deadline";
+  var $selectStatus = "select.status";
+  var $spanStatus = "span.status";
 
 
   // TRIGGERS
 
   // Add a row click
-  $(add_button).click(function(event){
+  $(toDoList).on("click", addButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
     addRow(el);
-    el.find("input").val("");
-    el.find("select").val("Not started");
+    emptyCurrentRow(el);
   });
 
   // Delete a row click
-  $(to_do_list).on("click", delete_button, function(event){
+  $(toDoList).on("click", deleteButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
     deleteRow(el);
   });
 
   // Edit a row click
-  $(to_do_list).on("click", edit_button, function(event){
+  $(toDoList).on("click", editButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
     editRow(el);
+  });
+
+  // Save a row click
+  $(toDoList).on("click", saveButton, function(event){
+    var el = $(this).closest("tr");
+    event.preventDefault();
+    var inputDesc = el.find($inputDescription).val();
+    var inputDeadline = el.find($inputDeadline).val();
+    var inputStatus = el.find($selectStatus).val();
+    saveRow(el, inputDesc, inputDeadline, inputStatus);
+  });
+
+  // Cancel an update to a row click
+  $(toDoList).on("click", cancelButton, function(event){
+    var el = $(this).closest("tr");
+    event.preventDefault();
+    cancelUpdate(el);
   });
 
 
@@ -41,13 +65,42 @@ $(document).ready(function(){
 
   // Add a row
 	function addRow(el){
-    var inputDesc = el.find("input#description").val();
-    var inputDeadline = el.find("input#deadline").val();
-    var inputStatus = el.find("select#status").val();
-    entry_count++;
-    //var tablerow = "<tr id='row" + entry_count + "'><td><input id='description' type='text' value='" + inputDesc + "' disabled/></td><td><input id='description' type='text' value='" + inputDeadline + "' disabled/></td><td>" + inputStatus + "</td><td><a data-role='edit-button' href='#'>Edit</a> | <a data-role='delete-button' href='#'>Delete</a></td></tr>";
-    var tablerow = $(to_do_list).find(row_template).html().replace(/^\s+|\s+$/, '');
-    $(to_do_list).append(tablerow);
+    var inputDesc = el.find($inputDescription).val();
+    var inputDeadline = el.find($inputDeadline).val();
+    var inputStatus = el.find($selectStatus).val();
+    entryCount++;
+    var tablerow = "<tr id='row" + entryCount + "'>\
+    <td>\
+      <span class='description'>" + inputDesc + "</span>\
+      <input class='description hidden' type='text' value='" + inputDesc + "'/></td>\
+    </td>\
+    <td>\
+      <span class='deadline'>" + inputDeadline + "</span>\
+      <input class='deadline hidden' type='text' value='" + inputDeadline + "'/></td>\
+    </td>\
+    <td>\
+      <span class='status'>" + inputStatus + "</span>\
+      <select class='status hidden'>\
+        <option value='Not started'>Not started</option>\
+        <option value='In progress'>In progress</option>\
+        <option value='Complete'>Complete</option>\
+      </select>\
+    <td>\
+      <a class='button btn-small' data-role='edit-button' href='#'>Edit</a>\
+      <a class='button btn-small' data-role='delete-button' href='#'>Delete</a>\
+      <a class='button btn-small hidden' data-role='save-button' href='#'>Save</a>\
+      <a class='button btn-small hidden' data-role='cancel-button' href='#'>Cancel</a>\
+    </td>\
+    </tr>";
+
+    $(toDoList).append(tablerow);
+    var $el = $('#row' + entryCount);
+    $el.find($selectStatus).val(inputStatus);
+  }
+
+  function emptyCurrentRow(el) {
+    el.find("input").val("");
+    el.find("select").val("Not started");
   }
 
   // Delete a row
@@ -57,7 +110,25 @@ $(document).ready(function(){
 
   // Edit a row
   function editRow(el) {
-    $(this).find("input[disabled]").removeAttr("disabled");
+    toggleRowStatus(el);
+  }
+
+  // Save a row
+  function saveRow(el, inputDesc, inputDeadline, inputStatus) {
+    el.find($spanDescription).html(inputDesc);
+    el.find($spanDeadline).html(inputDeadline);
+    el.find($spanStatus).html(inputStatus);
+    toggleRowStatus(el);
+  }
+
+  // Cancel update
+  function cancelUpdate(el) {
+    toggleRowStatus(el);
+  }
+
+  function toggleRowStatus(el) {
+    el.find("input, span, select").toggleClass('hidden');
+    el.find(buttons).toggleClass('hidden');
   }
 
 });
