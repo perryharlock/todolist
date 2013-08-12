ec0527009e3bbb923fbe0eb2ac49a9bd23156c0f
perryharlock <perry.harlock@tiscali.co.uk>
2013-08-12 16:11:05 +0100

    Added glyphicons, logic to disable adding empty rows

--- js/script.js
+++ js/script.js
@@ -3,64 +3,75 @@ $(document).ready(function(){
 	$("body.no-javascript").removeClass("no-javascript");
 
   // Elements
-  var $addButton = "[data-role='add-button']";
-  var $deleteButton = "[data-role='delete-button']";
-  var $editButton = "[data-role='edit-button']";
-  var $saveButton = "[data-role='save-button']";
-  var $cancelButton = "[data-role='cancel-button']";
-  var $buttons = ".button";
-  var $toDoList = "[data-role='to-do-list']";
-  var $entryCount = 0;
-  var $inputDescription = "input.description";
-  var $divDescription = "div.description";
-  var $inputDeadline = "input.deadline";
-  var $divDeadline = "div.deadline";
-  var $selectStatus = "select.status";
-  var $divStatus = "div.status";
+  var addButton = "[data-role='add-button']";
+  var deleteButton = "[data-role='delete-button']";
+  var editButton = "[data-role='edit-button']";
+  var saveButton = "[data-role='save-button']";
+  var cancelButton = "[data-role='cancel-button']";
+  var buttons = ".button";
+  var toDoList = "[data-role='to-do-list']";
+  var entryCount = 0;
+  var inputDescription = "input.description";
+  var divDescription = "div.description";
+  var inputNeededby = "input.deadline";
+  var divNeededby = "div.deadline";
+  var selectStatus = "select.status";
+  var divStatus = "div.status";
 
 
   // TRIGGERS
 
   // Add a row click
-  $($toDoList).on("click", $addButton, function(event){
+  $(toDoList).on("click", addButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
-    addRow(el);
-    emptyCurrentRow(el);
+    if (!$(el).find(addButton).hasClass('disabled')) {
+      addRow(el);
+      emptyCurrentRow(el);
+    }
   });
 
   // Delete a row click
-  $($toDoList).on("click", $deleteButton, function(event){
+  $(toDoList).on("click", deleteButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
     deleteRow(el);
   });
 
   // Edit a row click
-  $($toDoList).on("click", $editButton, function(event){
+  $(toDoList).on("click", editButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
     editRow(el);
   });
 
   // Save a row click
-  $($toDoList).on("click", $saveButton, function(event){
+  $(toDoList).on("click", saveButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
-    var inputDesc = el.find($inputDescription).val();
-    var inputDeadline = el.find($inputDeadline).val();
-    var inputStatus = el.find($selectStatus).val();
+    inputDesc = el.find(inputDescription).val();
+    inputDeadline = el.find(inputNeededby).val();
+    inputStatus = el.find(selectStatus).val();
     saveRow(el, inputDesc, inputDeadline, inputStatus);
   });
 
   // Cancel an update to a row click
-  $($toDoList).on("click", $cancelButton, function(event){
+  $(toDoList).on("click", cancelButton, function(event){
     var el = $(this).closest("tr");
     event.preventDefault();
-    var divDesc = el.find($divDescription).html();
-    var divDeadline = el.find($divDeadline).html();
-    var divStatus = el.find($divStatus).html();
+    divDesc = el.find(divDescription).html();
+    divDeadline = el.find(divNeededby).html();
+    divStatus = el.find(divStatus).html();
     cancelUpdate(el, divDesc, divDeadline, divStatus);
+  });
+
+  // On input values changing
+  $(inputDescription + ',' + inputNeededby + ',' + selectStatus).change(function() {
+    inputDescriptionValue = $(inputDescription).val();
+    inputDeadlineValue = $(inputNeededby).val();
+    selectStatusValue = $(selectStatus).val();
+    toggleAddButtonStatus(inputDescriptionValue, inputDeadlineValue, selectStatusValue);
+    console.log(inputDescriptionValue);
   });
 
 
@@ -68,11 +79,11 @@ $(document).ready(function(){
 
   // Add a row
 	function addRow(el){
-    var inputDesc = el.find($inputDescription).val();
-    var inputDeadline = el.find($inputDeadline).val();
-    var inputStatus = el.find($selectStatus).val();
-    $entryCount++;
-    var tablerow = "<tr class='added-item' id='row" + $entryCount + "'>\
+    var inputDesc = el.find(inputDescription).val();
+    var inputDeadline = el.find(inputNeededby).val();
+    var inputStatus = el.find(selectStatus).val();
+    entryCount++;
+    var tablerow = "<tr class='added-item' id='row" + entryCount + "'>\
     <td>\
       <div class='description'>" + inputDesc + "</div>\
       <input class='description hidden' type='text' value='" + inputDesc + "'/></td>\
@@ -89,21 +100,22 @@ $(document).ready(function(){
         <option value='Complete'>Complete</option>\
       </select>\
     <td>\
-      <a class='button btn-small' data-role='edit-button' href='#'>Edit</a>\
-      <a class='button btn-small' data-role='delete-button' href='#'>Delete</a>\
-      <a class='button btn-small hidden' data-role='save-button' href='#'>Save</a>\
-      <a class='button btn-small hidden' data-role='cancel-button' href='#'>Cancel</a>\
+      <a class='button btn-small' data-role='edit-button' href='#'><span class='glyphicon glyphicon-pencil'><span>Edit</span></span></a>\
+      <a class='button btn-small' data-role='delete-button' href='#'><span class='glyphicon glyphicon-remove'><span>Delete</span></span></a>\
+      <a class='button btn-small hidden' data-role='save-button' href='#'><span class='glyphicon glyphicon-ok'><span>Save</span></span></a>\
+      <a class='button btn-small hidden' data-role='cancel-button' href='#'><span class='glyphicon glyphicon-refresh'><span>Cancel</span></span></a>\
     </td>\
     </tr>";
 
-    $($toDoList).append(tablerow);
-    var $el = $('#row' + $entryCount);
-    $el.find($selectStatus).val(inputStatus);
+    $(toDoList).append(tablerow);
+    var $el = $('#row' + entryCount);
+    $el.find(selectStatus).val(inputStatus);
   }
 
   function emptyCurrentRow(el) {
     el.find("input").val("");
-    el.find("select").val("Not started");
+    el.find("select").val("");
+    el.find(addButton).addClass("disabled");
   }
 
   // Delete a row
@@ -122,24 +134,37 @@ $(document).ready(function(){
 
   // Save a row
   function saveRow(el, inputDesc, inputDeadline, inputStatus) {
-    el.find($divDescription).html(inputDesc);
-    el.find($divDeadline).html(inputDeadline);
-    el.find($divStatus).html(inputStatus);
+    el.find(divDescription).html(inputDesc);
+    el.find(divNeededby).html(inputDeadline);
+    el.find(divStatus).html(inputStatus);
     toggleRowStatus(el);
   }
 
   // Cancel update
   function cancelUpdate(el, divDesc, divDeadline, divStatus) {
-    el.find($inputDescription).val(divDesc);
-    el.find($inputDeadline).val(divDeadline);
-    el.find($selectStatus).val(divStatus);
+    el.find(inputDescription).val(divDesc);
+    el.find(inputNeededby).val(divDeadline);
+    el.find(selectStatus).val(divStatus);
     toggleRowStatus(el);
   }
 
+  //Toggle row buttons status
   function toggleRowStatus(el) {
     el.toggleClass('editable');
     el.find("input, div, select").toggleClass('hidden');
-    el.find($buttons).toggleClass('hidden');
+    el.find(buttons).toggleClass('hidden');
+  }
+
+  // Toggle the status of the add button on change of inputs
+  function toggleAddButtonStatus(inputDescriptionValue, inputDeadlineValue, selectStatusValue) {
+    if (inputDescriptionValue.length > 0 && inputDeadlineValue.length > 0 &&  selectStatusValue.length > 0) {
+        $(addButton).removeClass('disabled');
+    }
+    else {
+      if (!$(addButton).hasClass('disabled')) {
+        $(addButton).addClass('disabled');
+      }
+    }
   }
 
 });
